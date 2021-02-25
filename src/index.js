1dbe200fe04f3f8233a9d1f15ce78952746434cd
Oscar Barajas <oscar@dragons.mx>
2021-02-25 11:17:49 -0500

    :zap: [ADD] JS Portfolio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import sum from './utils/sum.js';
+import Template from './templates/Template.js';
+console.log('hola');
 
-const hello = 'hello';
-
-console.log(hello);
-console.log(sum(2, 2));
+(async function App() {
+  const main = null || document.getElementById('main');
+  main.innerHTML = await Template();
+})();
