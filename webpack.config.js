d94fed4f7d87bde7948cd341d086d60ff461e4af
Oscar Barajas <oscar@dragons.mx>
2021-02-25 16:06:03 -0500

    :art: [ADD] production

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -76,6 +77,7 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new CleanWebpackPlugin(),
   ],
   optimization: {
     minimize: true,
