b20263ba60351b2ad60e95366f2c227ccdbc5977
Oscar Barajas <oscar@dragons.mx>
2021-02-25 12:27:18 -0500

    :art: [ADD] CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,6 +19,13 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ],
       }
     ]
   },
@@ -26,6 +34,7 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new MiniCssExtractPlugin(),
   ]
 }
