14642009498a9ab99b68dea5c47435c694d39584
Oscar Barajas <oscar@dragons.mx>
2021-02-25 11:54:16 -0500

    :zap: [ADD] babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js']
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
 }
